54c27192155177fb59fb9a0006048d162d780548
stephendoyin <stevedoyin@gmail.com>
2020-11-17 11:48:28 +0100

    update

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,40 +1,15 @@
-import React, {useState, useRef, useEffect} from "react";
+import React from "react";
+import Select from "react-select";
 import "./Dropdown.scss";
-function Dropdown({value, onChange, options, prompt, label, id}) {
-  const [open, setOpen] = useState(false);
-  const ref = useRef(null);
 
-  useEffect(() => {
-    document.addEventListener("click", close);
-    return () => document.removeEventListener("click", close);
-  }, []);
-
-  function close(e) {
-    setOpen(e && e.target === ref.current);
-  }
-
+function Dropdown({options, placeholder}) {
   return (
     <div className="c-dropdown mb-4">
-      <div className="control" onClick={() => setOpen((prev) => !prev)}>
-        <div className="selected-value" ref={ref}>
-          {value ? value[label] : prompt}
-        </div>
-        <div className={`arrow ${open ? "open" : null}`} />
-      </div>
-      <div className={`options ${open ? "open" : null}`}>
-        {options.map((option) => (
-          <div
-            key={option[id]}
-            className={`option ${value === option ? "selected" : null}`}
-            onClick={() => {
-              setOpen(false);
-              onChange(option);
-            }}
-          >
-            {option[label]}
-          </div>
-        ))}
-      </div>
+      <Select
+        placeholder={placeholder + "..."}
+        classNamePrefix="mySelect"
+        options={options}
+      />
     </div>
   );
 }
--- src/components/LoanCalculator.js
+++ src/components/LoanCalculator.js
@@ -1,64 +1,25 @@
-import React, {useState} from "react";
+import React from "react";
 import Dropdown from "./Dropdown/Dropdown";
-import data from "../data/year.json";
-import dataTwo from "../data/ran.json";
+import year from "../data/year.json";
 import Fade from "react-reveal/Fade";
 
 function LoanCalculator() {
-  const prompt1 = "Select Year";
-  const prompt2 = "Car make";
-  // const prompt3 = "Car make";
-  // const prompt4 = "Body style";
-
-  const [value, setValue] = useState(null);
-  const [valueTwo, setValueTwo] = useState(null);
   return (
     <form className="form-restrict position-relative">
       <Fade bottom cascade>
         <div className="position-relative">
-          <Dropdown
-            options={data.reverse()}
-            label="year"
-            id="id"
-            prompt={prompt1}
-            value={value}
-            onChange={(val) => setValue(val)}
-          />
-        </div>
-
-        <div className="position-relative">
-          <Dropdown
-            options={dataTwo}
-            label="type"
-            id="id"
-            prompt={prompt2}
-            value={valueTwo}
-            onChange={(val) => setValueTwo(val)}
-          />
+          <Dropdown placeholder="Year" options={year.reverse()} />
         </div>
         <div className="position-relative">
-          <Dropdown
-            options={dataTwo}
-            label="type"
-            id="id"
-            prompt={prompt2}
-            value={valueTwo}
-            onChange={(val) => setValueTwo(val)}
-          />
+          <Dropdown placeholder="Make" options={year.reverse()} />
         </div>
         <div className="position-relative">
-          <Dropdown
-            options={dataTwo}
-            label="type"
-            id="id"
-            prompt={prompt2}
-            value={valueTwo}
-            onChange={(val) => setValueTwo(val)}
-          />
+          <Dropdown placeholder="Model" options={year.reverse()} />
+        </div>
+        <div className="position-relative">
+          <Dropdown placeholder="Body Style" options={year.reverse()} />
         </div>
       </Fade>
-      {/* <Dropdown options={data.reverse()} label="year" id="id" prompt={prompt3} value={value} onChange={val => setValue(val)} />
-            <Dropdown options={data.reverse()} label="year" id="id" prompt={prompt4} value={value} onChange={val => setValue(val)} /> */}
 
       <button className="btn px-5 mt-3 btn-orange">Get offer</button>
     </form>
