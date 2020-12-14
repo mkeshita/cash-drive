c12889e0d3322367cd62f2e0e5c0d559fa864f5f
Oluwaseyi Joshua <seyijosh44@gmail.com>
2020-12-14 17:52:11 +0100

    Encrypted the reference code of a loan offer for the integration of React app to the Nuxt app

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -23,7 +23,8 @@ function Hero() {
     const response = await Api.post(`${Api.ENDPOINTS.url}/estimate`, data);
     const {status} = response;
     if (status) {
-      window.location.href = `http://localhost:3000/register?token=${response.token}`;
+      const encoded = btoa(JSON.stringify(response));
+      window.location.href = `http://localhost:3000/register?token=${encoded}`;
     }
     setLoader(false);
   };
